d528d2767610dc306f06375ceb73199787835f13
Stephen Richard <smrTucson@gmail.com>
2025-10-21 16:34:10 -0700

    more cleanup, got teh local build to work

--- build.sh
+++ build.sh
@@ -12,3 +12,4 @@ fi
 docker run --pull=always --rm --workdir /workspace -v "$(pwd):/workspace" ${VOLUMES} \
   ghcr.io/opengeospatial/bblocks-postprocess \
   --clean true --base-url http://localhost:9090/register/
+
